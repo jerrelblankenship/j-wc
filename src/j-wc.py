6fba526ace5dceb882841970dbb1912a9491821f
Jerrel Blankenship <jerrelb@outlook.com>
2024-01-13 11:45:03 -0500

    Added the -l configuration that allows for determining the number of lines in the file.

--- src/j-wc.py
+++ src/j-wc.py
@@ -11,6 +11,7 @@ def main():
     # Define the arguments for the tool
     parser.add_argument('--ch', action='store_true', help='Special command')
     parser.add_argument('-c', nargs='?', const=sys.stdin, type=argparse.FileType('r'))
+    parser.add_argument('-l', type=argparse.FileType('r'))
 
 
     args = parser.parse_args()
@@ -20,6 +21,8 @@ def main():
             print(f"    {count_file_bytes(args.c.name)} {path.basename(args.c.name)}")
         else:
             print(f"    {count_file_bytes(args.c)}")
+    elif args.l:
+        print(f"    {count_file_lines(args.l.name)} {path.basename(args.l.name)}")
     elif args.ch:
         print("C is awesome!!!")
     else:
@@ -28,7 +31,9 @@ def main():
 def count_file_bytes(file_name):
     return path.getsize(file_name)
 
-
+def count_file_lines(file_name):
+    with open(file_name, "r") as file:
+        return sum(1 for line in file)
 
 if __name__ == "__main__":
     main()

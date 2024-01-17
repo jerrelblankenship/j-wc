f17c3ad397b94e60af0bdaf6721b2467c7d252fb
Jerrel Blankenship <jerrelb@outlook.com>
2024-01-16 23:21:51 -0500

    Finished up initial coding of the project.

--- src/j-wc.py
+++ src/j-wc.py
@@ -3,59 +3,60 @@
 from os import path, remove
 import sys
 import argparse
-from random import randint
 
 def main():
     parser = argparse.ArgumentParser(description="This script processes command line arguments.")
 
     # Define the arguments for the tool
     parser.add_argument('--ch', action='store_true', help='Special command')
-    parser.add_argument('-c', nargs='?', type=argparse.FileType('r'), const=sys.stdin)
-    parser.add_argument('-l', nargs='?', type=argparse.FileType('r'), const=sys.stdin)
-    parser.add_argument('-w', nargs='?', type=argparse.FileType('r'), const=sys.stdin)
-    parser.add_argument('-m', nargs='?', type=argparse.FileType('r'), const=sys.stdin)
-    parser.add_argument('filename', nargs='?')
+    parser.add_argument('-c', action='store_true', help='Count the number of bytes in the file or stdin')
+    parser.add_argument('-l', action='store_true', help='Count the number of lines in the file or stdin')
+    parser.add_argument('-w', action='store_true', help='Count the number of words in the file or stdin')
+    parser.add_argument('-m', action='store_true', help='Count the number of characters in the file or stdin')
+    parser.add_argument('filename', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
 
     args = parser.parse_args()
-    if args.ch == False and all(arg is None or (arg == sys.stdin and sys.stdin not in sys.argv) for arg in [args.c, args.l, args.w, args.m]):
-        print(f"  {count_file_lines(args.filename)}(lines) {count_file_words(args.filename)}(words) {count_file_bytes(args.filename)}(bytes) {path.basename(args.filename)}")
-    elif args.c:
-        if args.c.name != "<stdin>":
-            print(f"  {count_file_bytes(args.c.name)} {path.basename(args.c.name)}")
-        else:
-            print(f"  {count_file_bytes(args.c)}")
-    elif args.l:
-        print(f"  {count_file_lines(args.l.name)} {path.basename(args.l.name)}")
-    elif args.w:
-        print(f"  {count_file_words(args.w.name)} {path.basename(args.w.name)}")
-    elif args.m:
-        print(f"  {count_file_characters(args.m.name)} {path.basename(args.m.name)}")
-    elif args.ch:
-        print("C is awesome!!!")
+
+    # We need to determine if no arguments were passed in. If there were none, then execute the script as `-clw` has been passed in.
+    if not any([args.c, args.l, args.w, args.m, args.ch]):
+        args.c, args.l, args.w = True, True, True
+
+    source = args.filename
+    source_input = path.basename(source.name) if source != sys.stdin else "<stdin>"
+
+    if args.ch:
+        print(f"C is awesome!!!")
+    if args.c:
+        print(f"  {count_file_bytes(source)} {source_input}")
+        source.seek(0)
+    if args.l:
+        print(f"  {count_file_lines(source)} {source_input}")
+        source.seek(0)
+    if args.w:
+        print(f"  {count_file_words(source)} {source_input}")
+        source.seek(0)
+    if args.m:
+        print(f"  {count_file_characters(source)} {source_input}")
+
+def count_file_bytes(source):
+    if (source.name != "<stdin>"):
+        return path.getsize(source.name)
     else:
-        print("No arguments were passed in")
+        content = read_source_content(source)
+        return len(content.encode('utf-8'))
 
-def count_file_bytes(file_name):
-    return path.getsize(file_name)
+def count_file_lines(source):
+    return sum(1 for _ in source)
 
-def count_file_lines(file_name):
-    with open(file_name, "r") as file:
-        return sum(1 for line in file)
+def count_file_words(source):
+    return sum(len(line.split()) for line in source)
 
-def count_file_words(file_name):
-    word_count = 0
-    with open(file_name, "r") as file:
-        for line in file:
-            words = line.split()
-            word_count += len(words)
-    return word_count
+def count_file_characters(source):
+    return sum(len(chunk) for chunk in iter(lambda: source.read(1024), ''))
 
-def count_file_characters(file_name):
-    character_count = 0
-    with open(file_name, "r", encoding="utf-8") as file:
-        for chunk in iter(lambda: file.read(1024), ''):
-            character_count += len(chunk)
-    return character_count
+def read_source_content(source):
+    source.seek(0)  # Reset file pointer to the start
+    return source.read()
 
 if __name__ == "__main__":
     main()
